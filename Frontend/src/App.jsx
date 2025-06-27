3219db64f541d22cde07f6f0eca40683bef94786
sabhyasachisahoo <sabhyasachisahoo16@gmail.com>
2025-06-27 11:18:12 +0530

    Update API URL in App.js

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,70 +1,69 @@
-import { useState, useEffect } from 'react'
-import "prismjs/themes/prism-tomorrow.css"
-import Editor from "react-simple-code-editor"
-import prism from 'prismjs'
-import Markdown from 'react-markdown'
-import rehypeHighlight from 'rehype-highlight'
+import { useState, useEffect } from "react";
+import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
+import prism from "prismjs";
+import Markdown from "react-markdown";
+import rehypeHighlight from "rehype-highlight";
 import "highlight.js/styles/github-dark.css";
-import axios from 'axios'
-import './App.css'
+import axios from "axios";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [code, setCode] = useState(` function sum(){
   return 1+1
-}`)
+}`);
 
-const [review , setReview] = useState(``)
+  const [review, setReview] = useState(``);
 
-  useEffect(()=>{
-    prism.highlightAll()
-  },[])
+  useEffect(() => {
+    prism.highlightAll();
+  }, []);
 
-  async function reviewCode(){
+  async function reviewCode() {
+    const BASE_URL =
+      import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+    const response = await axios.post(`${BASE_URL}/ai/get-review`, { code });
 
-    const response = await axios.post('http://localhost:3000/ai/get-review', {code})
-    console.log(response.data)
+    // const response = await axios.post('http://localhost:3000/ai/get-review', {code})
+    console.log(response.data);
 
-    setReview(response.data)
+    setReview(response.data);
   }
 
   return (
     <>
-    <h1>YOUR BUDDY CODE REVIEWER</h1>
-    <main>
-      <div className="left">
-        <div className="code">
-          <Editor
-            value={code}
-            onValueChange={code => setCode(code)}
-            highlight={code => prism.highlight(code, prism.languages.javascript, "javascript")}
-            padding={10}
-            style={{
-              fontFamily: '"Fira code", "Fira Mono", monospace',
-              fontSize: 16,
-              // border: "1px solid #ddd",
-              borderRadius: "5px",
-              height: "100%",
-              width: "100%"
-            }}
-          />
+      <h1>YOUR BUDDY CODE REVIEWER</h1>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={(code) => setCode(code)}
+              highlight={(code) =>
+                prism.highlight(code, prism.languages.javascript, "javascript")
+              }
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                // border: "1px solid #ddd",
+                borderRadius: "5px",
+                height: "100%",
+                width: "100%",
+              }}
+            />
+          </div>
+          <div onClick={reviewCode} className="review">
+            Review
+          </div>
         </div>
-        <div
-          onClick={reviewCode}
-          className="review">Review</div>
-      </div>
-      <div className="right">
-        <Markdown
-
-          rehypePlugins={[ rehypeHighlight ]}
-
-        >{review}</Markdown>
-        
-      </div>
-    </main>
-  </>
-  )
+        <div className="right">
+          <Markdown rehypePlugins={[rehypeHighlight]}>{review}</Markdown>
+        </div>
+      </main>
+    </>
+  );
 }
 
-
-export default App
+export default App;
